3c5ef9aab941284ab83d4e41d6631630da0814e9
ruj <214782@bk.ru>
2021-07-12 13:54:01 -0300

    Stylization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,24 +6,25 @@ const Home: React.FC = () => {
   const { data } = useFetch('/api/repositories')
 
   return (
-    <div className="bg-dark-hard text-white font-mono flex h-screen">
+    <div className="bg-dark-hard text-white font-sans flex h-screen">
       <div className="m-auto">
-        <h1 className="text-3xl font-bold text-center">tentaclesoft</h1>
-        <div className="mt-10 grid grid-cols-2 gap-4">
+        <h1 className="text-3xl font-thin text-center">tentaclesoft</h1>
+        <div className="mt-4 grid grid-cols-2 gap-1">
           {data?.map((repository) => (
-            <div
+            <a
               key={repository.id}
-              className="max-w-xs border rounded-sm border-white border-opacity-10 bg-dark-soft p-3 space-y-3"
+              className="w-64 bg-dark-soft border border-transparent hover:border-gray-200 hover:border-opacity-25 p-3 space-y-3"
+              href={repository.url}
+              target="_blank"
+              rel="noreferrer"
             >
               <h3 className="text-white font-semibold text-md">
-                <a href={repository.url} target="_blank" rel="noreferrer">
-                  {repository.name}
-                </a>
+                {repository.name}
               </h3>
               <p className="text-xs text-justify text-gray-400 leading-sm">
                 {repository.description}
               </p>
-            </div>
+            </a>
           ))}
         </div>
       </div>
