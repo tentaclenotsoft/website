04cbea01725fb2fd454bc934a00411319e69a98f
ruj <214782@bk.ru>
2022-03-28 19:56:53 -0300

    feat: page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>tentaclesoft</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
